94f06fd299bdbf3eeb5d74d87fc96a3938dcca33
Rajkumar M <rajkumar.m@qxf2.com>
2020-02-04 21:56:27 +0530

    fixed the config error by updating the app.config in the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(app.config)
 
 #The import statment comes after the flask object is initialized to avoid circular reference
 from app import views
